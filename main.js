891d8f0e622eebfb3d5b0bec4a75cc9a1dd6cdeb
Cameron Green <camy624@comcast.net>
2023-11-01 12:45:41 -0400

    Fix deployment of cloned git repo on linode machine

--- main.js
+++ main.js
@@ -50,7 +50,7 @@ function cbSrchSubmit( xhttp ) {
 }
 
 function prepTblSrch( srchValue ) {
-    var qry = "/scripts/tbl_qry.php?srchValue=" + srchValue;
+    var qry = "/API/tbl_qry.php?srchValue=" + srchValue;
 
     localServerQry( "GET", qry, cbSrchSubmit);
 }
