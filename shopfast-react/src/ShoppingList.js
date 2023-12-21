e5313727885d5700a53b25a4108399c6df3b41c7
Cameron <cdg1039@usnh.edu>
2023-12-20 19:34:20 -0500

    Create main page UI for users

---
+++ shopfast-react/src/ShoppingList.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import './ShoppingList.css'; // Create this CSS file for styling
+
+const ShoppingList = () => {
+  const [items, setItems] = useState(['']); // Initial state with an empty item
+
+  const handleInputChange = (index, value) => {
+    const newItems = [...items];
+    newItems[index] = value;
+    setItems(newItems);
+  };
+
+  const handleAddItem = () => {
+    setItems([...items, '']);
+  };
+
+  const handleRemoveItem = (index) => {
+    const newItems = items.filter((_, i) => i !== index);
+    setItems(newItems);
+  };
+
+  return (
+    <div className="shopping-list">
+      {items.map((item, index) => (
+        <div key={index} className="list-item">
+          <input
+            type="text"
+            value={item}
+            onChange={(e) => handleInputChange(index, e.target.value)}
+          />
+          {index < items.length - 1 && (
+            <button className="trash-bin" onClick={() => handleRemoveItem(index)}>
+              🗑️
+            </button>
+          )}
+        </div>
+      ))}
+      <button className="add-item" onClick={handleAddItem}>
+        Add Item
+      </button>
+    </div>
+  );
+};
+
+export default ShoppingList;
