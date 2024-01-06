febe093f424049eafdef1b15bef1fe4fa307b5f8
Cameron Green <camy624@comcast.net>
2024-01-06 16:37:10 -0500

    Adjust build script for some bugs; still problem with moving folder in the end though

--- shopfast-react/build.sh
+++ shopfast-react/build.sh
@@ -1,5 +1,5 @@
-pointers_folder="${PROJECT_ROOT}/shopfast-react/api" # Grab the folder location that points with symbolic links
+pointers_folder="${PROJECT_ROOT}/shopfast-react/build/api" # Grab the folder location that points with symbolic links
 destination_pointers_folder="${PROJECT_ROOT}/shopfast-react/"
-sudo mv -P $pointers_folder $destination_pointers_folder # Temporarily move folder of symbolic links to avoid problems with npm
+sudo mv $pointers_folder $destination_pointers_folder # Temporarily move folder of symbolic links to avoid problems with npm
 sudo npm run build # Build the React application
-sudo mv -P $destination_pointers_folder $pointers_folder # Move folder of symbolic links back to proper/original position
+sudo mv $destination_pointers_folder $pointers_folder # Move folder of symbolic links back to proper/original position
