dfd90eb13bfe32ca93adbfced6ce29199a851fb8
Cameron Green <camy624@comcast.net>
2024-01-06 16:34:13 -0500

    Update build script to accommodate npm intolerance of extra, necessary folder containing symbolic links

--- shopfast-react/build.sh
+++ shopfast-react/build.sh
@@ -1,1 +1,5 @@
+pointers_folder="${PROJECT_ROOT}/shopfast-react/api" # Grab the folder location that points with symbolic links
+destination_pointers_folder="${PROJECT_ROOT}/shopfast-react/"
+sudo mv -P $pointers_folder $destination_pointers_folder # Temporarily move folder of symbolic links to avoid problems with npm
 sudo npm run build # Build the React application
+sudo mv -P $destination_pointers_folder $pointers_folder # Move folder of symbolic links back to proper/original position
