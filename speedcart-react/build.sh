4a106251ee66c2fee762b1cda75b66da14d53340
Cameron <cdg1039@usnh.edu>
2024-05-28 12:05:31 -0400

    Remove old links for previous API endpoints that were used prior to Laravel migration

--- speedcart-react/build.sh
+++ speedcart-react/build.sh
@@ -2,6 +2,3 @@ rm -rf "${PROJECT_ROOT}/speedcart-react/build"
 npm run build
 node "${PROJECT_ROOT}/speedcart-react/generateHTML.js"
 cd "${PROJECT_ROOT}/speedcart-react/build"
-mkdir api && cd api
-ln -s /var/www/SpeedCart/src/DataManager.php DataManager.php
-ln -s /var/www/SpeedCart/src/authenticate.php authenticate.php
