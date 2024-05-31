79c037af43b87ed23c40916c9bb60e5156a607ee
Cameron Green <camy624@comcast.net>
2024-05-30 20:02:40 -0400

    Start working on making footer positioning sticky

--- speedcart-react/src/components/Footer/Footer.js
+++ speedcart-react/src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import styles from './Footer.module.css';
 
 const Footer = (props) => {
   return (
-    <footer className="centerText" id={props.id}>
+    <footer className={`centerText ${styles.footer}`} id={props.id}>
       <a href="/terms-of-service.html" target="_blank" rel="noopener noreferrer" className={styles.rightWhiteBorder}>Terms of Service</a>
       <a href="/privacy-policy.html" target="_blank" rel="noopener noreferrer">Privacy Policy</a>
     </footer>
