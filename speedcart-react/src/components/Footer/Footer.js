76ad91bf31a3e0aa50a07dfad551f8d1014b91b2
Cameron <cdg1039@usnh.edu>
2024-06-01 15:21:08 -0400

    Adjust footer style and positioning for better visual design

--- speedcart-react/src/components/Footer/Footer.js
+++ speedcart-react/src/components/Footer/Footer.js
@@ -1,13 +1,18 @@
-// Footer.js
-import React from 'react';
-import styles from './Footer.module.css';
+import React from "react";
+import styles from "./Footer.module.css";
 
 const Footer = (props) => {
   return (
-    <footer className={`centerText ${styles.footer}`} id={props.id}>
-      <a href="/terms-of-service.html" target="_blank" rel="noopener noreferrer" className={styles.rightWhiteBorder}>Terms of Service</a>
-      <a href="/privacy-policy.html" target="_blank" rel="noopener noreferrer">Privacy Policy</a>
-    </footer>
+    <div className={styles.footerContainer}>
+      <footer className={`${styles.footer}`} id={props.id}>
+        <a href="/terms-of-service.html" target="_blank" rel="noopener noreferrer" className={styles.rightWhiteBorder}>
+          Terms of Service
+        </a>
+        <a href="/privacy-policy.html" target="_blank" rel="noopener noreferrer">
+          Privacy Policy
+        </a>
+      </footer>
+    </div>
   );
 };
 
