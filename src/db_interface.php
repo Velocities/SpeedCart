98c8e2fb092962a945f5e69e7f4b93765448d9e1
Cameron Green <camy624@comcast.net>
2024-06-02 12:07:22 -0400

    Remove unused code for previous site configurations

--- src/db_interface.php
+++
@@ -1,8 +0,0 @@
-<?php
-interface DatabaseInterface {
-    public function insertRecord($tblName, $data);
-    public function selectRecords($tblName, $params = []);
-    public function updateRecords($tblName, $data, $params = []);
-    public function deleteRecords($tblName, $params);
-}
-?>
