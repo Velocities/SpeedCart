354c7ae190a8b8457d82ce519ba53aaf40035ce2
Cameron Green <camy624@comcast.net>
2024-06-20 17:43:37 -0400

    Another change test for workflow

--- speedcart-react/build.sh
+++ speedcart-react/build.sh
@@ -4,4 +4,5 @@ rm -rf "${PROJECT_ROOT}/speedcart-react/build"
 npm run build
 # Rebuild HTML for ToS and Privacy Policy files
 node "${PROJECT_ROOT}/speedcart-react/generateHTML.js"
+# Go to build directory for any user needs (e.g. check on build files, permissions, etc.)
 cd "${PROJECT_ROOT}/speedcart-react/build"
