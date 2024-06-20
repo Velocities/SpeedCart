99faed454921ecd4ca5521776a6b33f0152d1cf9
Cameron Green <camy624@comcast.net>
2024-06-20 17:56:25 -0400

    Adjust file path for workflow execution

--- speedcart-react/generateHTML.js
+++ speedcart-react/generateHTML.js
@@ -9,7 +9,7 @@ const generateHTML = (inputFile, outputFile) => {
 };
 
 // Define paths relative to the project root
-const PROJECT_ROOT = process.env.PROJECT_ROOT || '.';
+const PROJECT_ROOT = process.env.PROJECT_ROOT || '/var/www/SpeedCart';
 const LEGAL_DOCS_FOLDER = `${PROJECT_ROOT}/docs/legal`;
 const REACT_BUILD_FOLDER = `${PROJECT_ROOT}/speedcart-react/build`;
 
