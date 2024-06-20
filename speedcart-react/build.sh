81ae5f31e82e54f93d3f0f69a961c7200bc74e27
Cameron Green <camy624@comcast.net>
2024-06-20 17:05:20 -0400

    Update old functional build script with comments

--- speedcart-react/build.sh
+++ speedcart-react/build.sh
@@ -1,4 +1,7 @@
+# Remove old build contents
 rm -rf "${PROJECT_ROOT}/speedcart-react/build"
+# Rebuild React app
 npm run build
+# Rebuild HTML for ToS and Privacy Policy files
 node "${PROJECT_ROOT}/speedcart-react/generateHTML.js"
 cd "${PROJECT_ROOT}/speedcart-react/build"
