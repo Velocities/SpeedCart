ef7c100afc622ee6a3b09e75e8f3711df38e505c
Cameron <cdg1039@usnh.edu>
2024-06-28 15:50:21 -0400

    First additions and attempts at Dashboard UI changes for Issue #44 implementations

--- speedcart-react/src/pages/Dashboard/Dashboard.js
+++ speedcart-react/src/pages/Dashboard/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { FaTrash } from 'react-icons/fa';
+import { FaTrash, FaEdit } from 'react-icons/fa';
 import dashboardStyles from './Dashboard.module.css';
 import layoutStyles from '../main.module.css'; // Import the new layout styles
 
@@ -44,6 +44,10 @@ function Dashboard() {
         })
         .then(data => {
             setShoppingListTitles(data);
+            console.log("data returned: ");
+            for (let i = 0; i < data.length; i++) {
+                console.log("item " + i + ": " + JSON.stringify(data[0]));
+            }
             setIsLoading(false);
         })
         .catch(error => {
@@ -135,11 +139,18 @@ function Dashboard() {
                         <ul>
                             {filteredShoppingListTitles.map(list => (
                                 <li key={list.list_id} className={dashboardStyles.shoppingListItem}>
-                                    <Link to={`/shopping-list/${list.list_id}`}>{list.name}</Link>
-                                    <FaTrash 
-                                        className={dashboardStyles.deleteIcon}
-                                        onClick={() => handleDelete(list.list_id)}
-                                    />
+                                    <Link to={`/shopping-list/${list.list_id}`} className={dashboardStyles.iconContainer}>
+                                        <FaEdit className={dashboardStyles.viewEditIcon} />
+                                        <span className={dashboardStyles.tooltip}>View/Edit List</span>
+                                    </Link>
+                                    <span className={dashboardStyles.listName}>{list.name}</span>
+                                    <div className={dashboardStyles.listDetails}>
+                                        <span className={dashboardStyles.updatedAt}>{new Date(list.updated_at).toLocaleString()}</span>
+                                        <FaTrash 
+                                            className={dashboardStyles.deleteIcon}
+                                            onClick={() => handleDelete(list.list_id)}
+                                        />
+                                    </div>
                                 </li>
                             ))}
                         </ul>
