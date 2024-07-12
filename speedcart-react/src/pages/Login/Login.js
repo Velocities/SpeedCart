73cf7d9d8897c8822af907ced341f6513cb6182d
Cameron <cdg1039@usnh.edu>
2024-07-12 16:42:27 -0400

    Remove previous debugging code for Login jwt

--- speedcart-react/src/pages/Login/Login.js
+++ speedcart-react/src/pages/Login/Login.js
@@ -1,26 +1,12 @@
-import React, { useLayoutEffect, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { GoogleLogin } from '@react-oauth/google';
-import { jwtDecode } from 'jwt-decode';
 
 import { useAuth } from '@customHooks/AuthContext';
 
 import styles from './Login.module.css';
 
-const debug = 0;
-
 function Login() {
   const { isAuthenticated, login, logout} = useAuth();
-  
-
-  useLayoutEffect(() => {
-    // Check if the user is already authenticated (e.g., stored in localStorage)
-    const storedToken = localStorage.getItem('authToken');
-    if (storedToken && debug) {
-      // Decode the token to get user information
-      const decodedToken = jwtDecode(JSON.parse(storedToken).credential);
-      console.log('User is already authenticated:', decodedToken);
-    }
-  }, []);
 
   useEffect(() => {
     if ( isAuthenticated ) { 
