37cc2fbac4b90043fe337fc47f0bae4848906b0b
unknown <camy624@comcast.net>
2024-09-22 17:46:15 -0400

    First semi-working version of TypeScript in shared

--- Frontend/shared/src/constants/BaseUrl.ts
+++ Frontend/shared/src/constants/BaseUrl.ts
@@ -1,5 +1,4 @@
 // src/constants/config/config.ts
 
 // This can be reused for all backend interactions
-const BASE_URL: string = `https://${process.env.REACT_APP_API_DOMAIN}:${process.env.REACT_APP_API_PORT}`;
-export default BASE_URL;
+export const BASE_URL: string = `https://${process.env.REACT_APP_API_DOMAIN}:${process.env.REACT_APP_API_PORT}`;
