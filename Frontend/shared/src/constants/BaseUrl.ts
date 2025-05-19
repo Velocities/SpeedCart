193982aac5a2f7299ebe7c6dd7ccc6f89edf445d
unknown <camy624@comcast.net>
2025-05-19 19:38:54 -0400

    remove: unnecessary API_PORT from Frontend shared package code now that DigitalOcean hosts it on HTTPS automatically

--- Frontend/shared/src/constants/BaseUrl.ts
+++ Frontend/shared/src/constants/BaseUrl.ts
@@ -2,11 +2,10 @@
 import config from './config.json';
 
 const API_DOMAIN = config.API_DOMAIN;
-const API_PORT = config.API_PORT;
 
 // Protocol depends on if we're using dev or production
 const API_PROTOCOL = API_DOMAIN === 'localhost' ? 'http' : 'https';
 
 // This can be reused for all backend interactions
-export const BASE_URL: string = `${API_PROTOCOL}://${API_DOMAIN}:${API_PORT}`;
+export const BASE_URL: string = `${API_PROTOCOL}://${API_DOMAIN}`;
 export const TESTING_MODE: boolean = config.TESTING_MODE === 'true';
